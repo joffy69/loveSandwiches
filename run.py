858f9ead3113b1e84230ac22fd3ebb8d22831d27
joffy69 <jcwgibbs@gmail.com>
2021-11-01 16:32:02 +0000

    user data validation

--- run.py
+++ run.py
@@ -21,6 +21,24 @@ def get_sales_data():
     print('example: 10,19,10,2,43,8\n')
 
     data_str = input("Enter data here: ")
-    print(f'the data provided is {data_str}')
+   
+    sale_data = data_str.split(',')
+    validate_data(sale_data)
+
+
+def validate_data(values):
+    """
+    inside the try, converts all str values into integers
+    raises valueerror if str cannot be converted into int, 
+    or if there arent exactly 6 values
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f'exactly 6 values, you provided {len(values)}'
+            )
+    except ValueError as e:
+        print(f'invalid data: {e}. please try again\n')
+        print(values)
 
 get_sales_data()
